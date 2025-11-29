a6356f765b5337674eef75f02e2572499c30ffd3
jaarzaga1721 <jemierynarzaga08@gmail.com>
2025-11-29 20:46:46 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> The Beauty Store | Jemieryn Arzaga CYB-201 - All Rights Reserved. </p>
+</footer>
+</html>
